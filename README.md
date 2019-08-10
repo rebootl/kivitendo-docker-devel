3bf9205a09d99019c2a507ff08eff95f818b85ef
Cem Aydin <cem.aydin@gmx.ch>
2019-08-10 09:58:00 +0200

    adapt right only on first container startup, adapt readme

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 docker development environment for kivitendo-erp incl. postgres database.
 
+## Kivitendo setup (default github repo.)
+
+  $ cd kivi/
+  $ git clone https://github.com/kivitendo/kivitendo-erp.git
+  $ cd kivitendo-erp/
+  $ mkdir webdav
+
+The entrypoint script chowns the user to www-data and sets the group to rw
+on first container startup.
+
 ## docker-compose
 
   $ docker-compose build
--- kivi/docker-entrypoint.sh
+++ kivi/docker-entrypoint.sh
@@ -3,10 +3,20 @@
 #
 
 
-# set kivitendo rw
-cd /var/www/kivitendo-erp
-chown -R www-data users spool webdav
-chown www-data templates
+# set kivitendo rw on first container startup
+STARTED_LOCK_FILE="/started.lock"
+if [ ! -e $STARTED_LOCK_FILE ]; then
+  echo "-- First container startup --"
+  cd /var/www/kivitendo-erp
+  chown -R www-data users spool webdav
+  chmod -R 775 users spool webdav
+  chown www-data templates
+  chmod 775 templates
+
+  touch $STARTED_LOCK_FILE
+else
+  echo "-- Not first container startup --"
+fi
 
 source /etc/apache2/envvars
 echo "$APACHE_RUN_DIR"
