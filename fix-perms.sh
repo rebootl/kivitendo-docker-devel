465312e73e80b7c224c5afb7b796e13750098b31
Cem Aydin <cem.aydin@gmx.ch>
2019-11-11 20:14:21 +0100

    update path

--- fix-perms.sh
+++ fix-perms.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find kivi/kivitendo-erp/users/templates-cache/templates/webpages/ -type f |xargs sudo chmod 644
+find web/kivitendo-erp/users/templates-cache/templates/webpages/ -type f |xargs sudo chmod 644
