2e12336a51608ffe21fa7a012eea4100ad09b858
Cem Aydin <cem.aydin@gmx.ch>
2024-02-08 15:34:00 +0100

    rem. db/docker-entrypoint-initdb.d/01-init.sh

--- db/docker-entrypoint-initdb.d/01-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    /* add roles if neccessary */
-    /*CREATE ROLE mykiviuser WITH SUPERUSER;*/
-EOSQL
